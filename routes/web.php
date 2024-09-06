20912b86fe4ac88ca43de5122c1c9ee08d367ffb
Zayar-Win <zayarwin7751@gmail.com>
2024-09-06 14:47:42 +0700

    done-checkout-page

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::get('/products/t-shirt', function () {
     return Inertia::render('ProductDetail');
 });
 
+Route::get('/checkout', function () {
+    return Inertia::render('Checkout');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
